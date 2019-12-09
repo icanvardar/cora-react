4a7a4eca9490cf3f09b8e331ada91435e3de4fd1
Can Vardar <ismailcanvardar@gmail.com>
2019-12-09 20:14:15 +0300

    second attempt

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 export default () => {
     return (
-        <h1>This is Home Page Component!</h1>
+        <div>
+            <Link to='/selective'>Seçim Ekranı</Link>
+            <Link to='/login'>Giriş Ekranı</Link>
+            <Link to='/register'>Kayıt Ekranı</Link>
+            <h1>This is Home Page Component!</h1>
+        </div>
     )
 }
